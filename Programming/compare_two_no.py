0c3d1d806360f48d7ee87b73a93791632f0ea9ec
asma-khatoon <asmakhatoon165@gmail.com>
2025-06-23 04:01:11 +0530

    Compared two numbers

---
+++ Programming/compare_two_no.py
@@ -0,0 +1,11 @@
+print("Enter first number")
+n1 = int(input())
+print("Enter second number")
+n2 =int(input())
+if n1>n2:
+    print("n1 is greater than n2")
+else:
+    if n2>n1:
+        print("n2 is greater than n1")
+    else:
+        print("both numbers are equal")
