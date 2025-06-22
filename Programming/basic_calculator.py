4b4354e889a5a435b0d680b5e8734a2714a3b404
asma-khatoon <asmakhatoon165@gmail.com>
2025-06-22 18:52:39 +0530

    created simple calculation program

---
+++ Programming/basic_calculator.py
@@ -0,0 +1,18 @@
+print("My first calculator program")
+print("Enter first number: ")
+num1 = int((input()))
+
+print("Enter second number: ")
+num2 = int(input())
+
+sum = num1 + num2
+print("Sum of two numbers:", sum)
+
+sub =num1 -num2
+print("Substraction of two number:", sub)
+
+mul = num1*num2
+print ("multiplication of two numbers:", mul)
+
+div = num1/num2
+print("Division of two numbers:", div)
