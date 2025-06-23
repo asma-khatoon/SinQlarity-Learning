cfc6e97d17b3d94b44f5554eaeb22838e5f11e59
asma-khatoon <asmakhatoon165@gmail.com>
2025-06-23 16:42:28 +0530

    Implemented array list using while loop

---
+++ Programming/list.py
@@ -0,0 +1,18 @@
+print("marks of each student")
+marks =[12,23,4,5,67,34,67,22]
+i=0
+while i<8:
+    print(i,"-", marks[i])
+    i=i+1
+
+
+print("marks of first 5 students incremented by 15")
+i=0
+while i<5:
+    marks[i] += 15
+    
+    print(i, "-" , marks[i])
+    i=i+1
+
+
+
