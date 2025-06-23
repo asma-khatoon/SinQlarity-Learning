7fdbc9b50a57cfa3ab3271dc0fa167e0c5f62fbf
asma-khatoon <asmakhatoon165@gmail.com>
2025-06-23 13:19:59 +0530

    implemented for loop with range property

---
+++ Programming/range.py
@@ -0,0 +1,12 @@
+print("enter no:")
+num =int(input())
+print("start from 1 and gap is 3 and end of range is  num")
+for i in range(1,num,3): # when start number, end number and gap between every no is given
+    print(i)
+    
+for i in range(3,num):
+    print(i)
+   
+
+for i in range(num):
+    print(i)
