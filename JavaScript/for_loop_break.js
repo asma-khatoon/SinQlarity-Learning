74f14fbf05f9443406c2f9fa3295ab5c49968395
asma-khatoon <asmakhatoon165@gmail.com>
2025-06-26 17:36:20 +0530

    Implemented for loop with break statement

---
+++ JavaScript/for_loop_break.js
@@ -0,0 +1,11 @@
+let n=5;
+for(let i=1; i<=n; i++){
+    for(let j=1;j<=n;j++){
+        for(let k=0;k<n;k++){
+           console.log(i,j,k);
+        }
+    }
+    if(i=3){
+        break;
+    }
+}
