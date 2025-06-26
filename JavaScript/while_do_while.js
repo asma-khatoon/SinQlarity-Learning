9f1a40ad0900f2c57d4f51cdc2cac4291492f2c0
asma-khatoon <asmakhatoon165@gmail.com>
2025-06-26 16:40:24 +0530

    Implemented difference in do while and while

---
+++ JavaScript/while_do_while.js
@@ -0,0 +1,23 @@
+let num=1;
+while(num<=10){
+    console.log("num=",num*num);
+    num++;
+}
+console.log("loop is over");
+
+let num2=1;
+do{
+    console.log("num=",num2*num2);
+    num2++;
+}
+while(num2<=10);
+console.log("loop is over");
+
+let num3=12;
+do{
+    console.log("num=",num3*num3);
+    num3++;
+}
+while(num3<=10);
+console.log("loop is over");
+
