1f156f8b25af3e67d18fa2c959680947f48a3ea9
asma-khatoon <asmakhatoon165@gmail.com>
2025-06-26 19:25:51 +0530

    implemented function expression in js

---
+++ JavaScript/function_expression.js
@@ -0,0 +1,12 @@
+const getSquare= function(num){
+    console.log("square of number is:", num*num);
+    return num*num;
+}
+let n =25;
+getSquare(n);
+
+const getSqr = function square(n1){
+    console.log( `square of number is: ${n1*n1}`);
+}
+let n1 =6;
+getSqr(n1);
