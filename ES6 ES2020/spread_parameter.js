ea63868a1518d8abfcf60364d043b4322b30d966
asma-khatoon <asmakhatoon165@gmail.com>
2025-06-30 13:44:51 +0530

    created spread parameters

---
+++ ES6 ES2020/spread_parameter.js
@@ -0,0 +1,11 @@
+let arr =[1,2,4,6,7];
+let arr1 = [2,5,7,8,9,0,3];
+let combined_arr = [...arr, ...arr1];
+let new_array =[arr,arr1];
+let objStudent ={sname:"ABC", age:30}
+let objStudent1 = objStudent;
+console.log(combined_arr);
+console.log(new_array);
+console.log(...arr);
+console.log(objStudent);
+console.log(objStudent1);
