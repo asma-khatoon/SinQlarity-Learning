9a44991468510062d9d2c19210938850dd29faaf
asma-khatoon <asmakhatoon165@gmail.com>
2025-06-30 11:50:19 +0530

    Created uses of array function

--- ES6 ES2020/arrow_func.js
+++ ES6 ES2020/arrow_func.js
@@ -1,2 +1,7 @@
+let arr = [1,2,3,5,6,7];
+arr.map(function(element){
+    return element * 4;
+})
+console.log(arr);
 const obj= (user_name, age)=>({user_name: user_name, age:age})
 console.log(obj("ABC", 30));
