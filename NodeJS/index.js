b7a52ea9aa9200c8f6f2b1516aa95dddb8a0be17
asma-khatoon <asmakhatoon165@gmail.com>
2025-07-03 16:46:55 +0530

    Created basic calculator using nodejs

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -1,58 +1,20 @@
 const express = require('express');
 const app = express();
+const calculatorRouter = require('./routers/calculator');
 app.use(express.json());
 app.get('/',(req,res)=>{
     //req.send("I am request");
     res.send("I am response");
     
 });
-app.get('/add',(req,res)=>{
-    let n1= 10;
-    let n2= 30;
-    let sum= n1+n2;
-    res.send(`sum =${sum}`);
+// app.get('/add',(req,res)=>{
+//     let n1= 10;
+//     let n2= 30;
+//     let sum= n1+n2;
+//     res.send(`sum =${sum}`);
     
-});
-app.post('/add',(req,res)=>{
-    //console.log(req.body);
-    let n1= req.body.num1;
-    let n2 = req.body.num2;
-    let sum=0;
-    sum= n1+n2;
-    res.send(`sum =${sum}`);
-    //console.log(sum);
+// });
 
-});
-app.post('/sub',(req,res)=>{
-    
-    let n1= req.body.num1;
-    let n2 = req.body.num2;
-    let sub=0;
-    sub= n1-n2;
-    res.send(`sub =${sub}`);
-   
-
-});
-app.post('/mul',(req,res)=>{
-
-    let n1= req.body.num1;
-    let n2 = req.body.num2;
-    let mul=0;
-    mul= n1*n2;
-    res.send(`multiplication =${mul}`);
-    
-
-});
-app.post('/divide',(req,res)=>{
-   
-    let n1= req.body.num1;
-    let n2 = req.body.num2;
-    let div=0;
-    div= n1/n2;
-    res.send(`division=${div}`);
-    
-
-});
-
+app.use('/calculator', calculatorRouter);
 app.listen(5500);
 
