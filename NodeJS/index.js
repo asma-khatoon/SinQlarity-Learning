1e55e22bb77a0d79f18eb0999637fc47c176e086
asma-khatoon <asmakhatoon165@gmail.com>
2025-07-03 11:01:55 +0530

    Created calculator using rest API

--- NodeJS/index.js
+++ NodeJS/index.js
@@ -14,10 +14,45 @@ app.get('/add',(req,res)=>{
     
 });
 app.post('/add',(req,res)=>{
-    console.log(req.body);
+    //console.log(req.body);
+    let n1= req.body.num1;
+    let n2 = req.body.num2;
     let sum=0;
+    sum= n1+n2;
     res.send(`sum =${sum}`);
+    //console.log(sum);
 
 });
+app.post('/sub',(req,res)=>{
+    
+    let n1= req.body.num1;
+    let n2 = req.body.num2;
+    let sub=0;
+    sub= n1-n2;
+    res.send(`sub =${sub}`);
+   
+
+});
+app.post('/mul',(req,res)=>{
+
+    let n1= req.body.num1;
+    let n2 = req.body.num2;
+    let mul=0;
+    mul= n1*n2;
+    res.send(`multiplication =${mul}`);
+    
+
+});
+app.post('/divide',(req,res)=>{
+   
+    let n1= req.body.num1;
+    let n2 = req.body.num2;
+    let div=0;
+    div= n1/n2;
+    res.send(`division=${div}`);
+    
+
+});
+
 app.listen(5500);
-//app.listen(5500);
+
