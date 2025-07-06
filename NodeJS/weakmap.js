94ade1b9bd6fbc35a8759d44e0b5bd6b347bcbbe
asma-khatoon <asmakhatoon165@gmail.com>
2025-07-06 15:59:46 +0530

    Implemented weakmap concept

---
+++ NodeJS/weakmap.js
@@ -0,0 +1,11 @@
+let person={
+    name1: "Mr A",
+    named2: "Mr B"
+}
+let key1={};
+let key2 = {name1:"Meera"};
+let weakMap ={
+    [key1]:"data1",
+    [key2]:"data2"
+}
+console.log(weakMap);
